8c5f5319ebcef4d2fbbefea19ad341ec75a71436
Aris Saputra <arissaputra362@gmail.com>
2023-03-08 23:53:10 +0700

    jurnal otomatis penjualan

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -65,8 +65,8 @@ class ApiController extends Controller
             }
 
             $akun_slip = $data_slip->id_akun;
-            $akun_uang_muka_penjualan = $data_akun_uang_muka_penjualan->value1;
-            $akun_ppn_keluaran = $data_akun_ppn_keluaran->value1;
+            $akun_uang_muka_penjualan = $data_akun_uang_muka_penjualan->value2;
+            $akun_ppn_keluaran = $data_akun_ppn_keluaran->value2;
             $total = $request->total;
             $uang_muka = $request->uang_muka;
             $nominal_ppn = $request->ppn;
@@ -108,6 +108,7 @@ class ApiController extends Controller
             } else {
                 if(!empty($header) && $header->id_slip != $id_slip){
                     $header->void = 1;
+                    $header->save();
                 }
                 $header = new JurnalHeader();
                 $header->id_cabang = $id_cabang;
@@ -227,18 +228,18 @@ class ApiController extends Controller
                 ]);
             }
 
-            $data_akun_ppn_masukan = DB::table('setting')->where('id', 'PPN Masukan')->where('tipe', 2)->where('id_cabang', $id_cabang)->first();
+            $data_akun_ppn_masukan = DB::table('setting')->where('id', 'PPN Masukkan')->where('tipe', 2)->where('id_cabang', $id_cabang)->first();
             if(empty($data_akun_ppn_masukan)){
                 return response()->json([
                     "result" => false,
                     "code" => 404,
-                    "message" => "Error, Setting PPN Masukan not found"
+                    "message" => "Error, Setting PPN Masukkan not found"
                 ]);
             }
 
             $akun_slip = $data_slip->id_akun;
-            $akun_uang_muka_pembelian = $data_akun_uang_muka_pembelian->value1;
-            $akun_ppn_masukan = $data_akun_ppn_masukan->value1;
+            $akun_uang_muka_pembelian = $data_akun_uang_muka_pembelian->value2;
+            $akun_ppn_masukan = $data_akun_ppn_masukan->value2;
             $total = $request->total;
             $uang_muka = $request->uang_muka;
             $nominal_ppn = $request->ppn;
@@ -260,7 +261,7 @@ class ApiController extends Controller
                     'akun'          => $akun_ppn_masukan,
                     'debet'         => 0,
                     'credit'        => $nominal_ppn,
-                    'keterangan'    => 'Jurnal Otomatis PPN Masukan - ' . $id_transaksi . ' - ' . $nama_pelanggan,
+                    'keterangan'    => 'Jurnal Otomatis PPN Masukkan - ' . $id_transaksi . ' - ' . $nama_pelanggan,
                 ],
             ];
 
@@ -280,6 +281,7 @@ class ApiController extends Controller
             } else {
                 if(!empty($header) && $header->id_slip != $id_slip){
                     $header->void = 1;
+                    $header->save();
                 }
                 $header = new JurnalHeader();
                 $header->id_cabang = $id_cabang;
@@ -356,15 +358,330 @@ class ApiController extends Controller
         }
     }
 
-    public function generateJournalCode($cabang, $jenis, $slip)
+    public function journalPenjualan(Request $request)
+    {
+        // try {
+            Log::debug(json_encode($request->all()));
+            // init data
+            // header
+            $id_transaksi = $request->no_transaksi;
+            $tanggal_jurnal = date('Y-m-d', strtotime($request->tanggal));
+            $void = $request->void;
+            $user_created = $request->user;
+            $id_pelanggan = $request->pelanggan;
+            $id_cabang = $request->cabang;
+            $id_slip = $request->slip;
+            $detail_inventory = $request->detail;
+
+            $data_pelanggan = DB::table("pelanggan")->where('id_pelanggan', $id_pelanggan)->first();
+            Log::debug($id_pelanggan);
+            Log::debug(json_encode($data_pelanggan));
+            $nama_pelanggan = $data_pelanggan->nama_pelanggan;
+            $catatan_me = 'Journal Otomatis Penjualan - ' . $id_transaksi . ' - ' . $nama_pelanggan;
+
+            // init setting
+            $data_akun_piutang_dagang = DB::table('setting')->where('id', 'Piutang Dagang')->where('tipe', 2)->where('id_cabang', $id_cabang)->first();
+            if(empty($data_akun_piutang_dagang)){
+                return response()->json([
+                    "result" => false,
+                    "code" => 404,
+                    "message" => "Error, Setting Piutang Dagang not found"
+                ]);
+            }
+
+            $data_akun_uang_muka_penjualan = DB::table('setting')->where('id', 'UM Penjualan')->where('tipe', 2)->where('id_cabang', $id_cabang)->first();
+            if(empty($data_akun_uang_muka_penjualan)){
+                return response()->json([
+                    "result" => false,
+                    "code" => 404,
+                    "message" => "Error, Setting UM Penjualan not found"
+                ]);
+            }
+
+            $data_akun_ppn_keluaran = DB::table('setting')->where('id', 'PPN Keluaran')->where('tipe', 2)->where('id_cabang', $id_cabang)->first();
+            if(empty($data_akun_ppn_keluaran)){
+                return response()->json([
+                    "result" => false,
+                    "code" => 404,
+                    "message" => "Error, Setting PPN Keluaran not found"
+                ]);
+            }
+
+            $data_akun_penjualan = DB::table('setting')->where('id', 'Penjualan')->where('tipe', 2)->where('id_cabang', $id_cabang)->first();
+            if(empty($data_akun_penjualan)){
+                return response()->json([
+                    "result" => false,
+                    "code" => 404,
+                    "message" => "Error, Setting Penjualan not found"
+                ]);
+            }
+
+
+            // detail
+            // Memorial
+            $akun_piutang_dagang = $data_akun_piutang_dagang->value2;
+            $akun_uang_muka_penjualan = $data_akun_uang_muka_penjualan->value2;
+            $akun_ppn_keluaran = $data_akun_ppn_keluaran->value2;
+            $akun_penjualan = $data_akun_penjualan->value2;
+            $total = $request->total;
+            $uang_muka = $request->uang_muka;
+            $nominal_ppn = $request->ppn;
+
+            $jurnal_detail_me = [
+                [
+                    'akun'          => $akun_piutang_dagang,
+                    'debet'         => ($total + $uang_muka),
+                    'credit'        => 0,
+                    'keterangan'    => 'Jurnal Otomatis Penjualan ' . $id_transaksi . ' - ' . $nama_pelanggan,
+                    'id_transaksi'  => null
+                ],
+                [
+                    'akun'          => $akun_ppn_keluaran,
+                    'debet'         => 0,
+                    'credit'        => $nominal_ppn,
+                    'keterangan'    => 'Jurnal Otomatis PPN Keluaran - ' . $id_transaksi . ' - ' . $nama_pelanggan,
+                    'id_transaksi'  => null
+                ],
+                [
+                    'akun'          => $akun_uang_muka_penjualan,
+                    'debet'         => $uang_muka,
+                    'credit'        => 0,
+                    'keterangan'    => 'Jurnal Otomatis Uang Muka Penjualan - ' . $id_transaksi . ' - ' . $nama_pelanggan,
+                    'id_transaksi'  => null
+                ],
+                [
+                    'akun'          => $akun_piutang_dagang,
+                    'debet'         => 0,
+                    'credit'        => $uang_muka,
+                    'keterangan'    => 'Jurnal Otomatis Pelunasan - ' . $id_transaksi . ' - ' . $nama_pelanggan,
+                    'id_transaksi'  => $id_transaksi
+                ],
+            ];
+
+            foreach($detail_inventory as $d_inv){
+                array_push($jurnal_detail_me, [
+                    'akun'          => $akun_penjualan,
+                    'debet'         => 0,
+                    'credit'        => $d_inv->total,
+                    'keterangan'    => 'Jurnal Otomatis Pelunasan - ' . $id_transaksi . ' - ' . $nama_pelanggan . ' - ' . $d_inv->nama_barang,
+                    'id_transaksi'  => null
+                ]);
+            }
+
+            // Find Header data and delete detail
+            $header = JurnalHeader::where("id_transaksi", $id_transaksi)->where('void', 0)->first();
+
+            // Begin save
+            DB::beginTransaction();
+            if (!empty($header)) {
+                JurnalDetail::where('id_jurnal', $header->id_jurnal)->delete();
+                $header->id_cabang = $id_cabang;
+                $header->tanggal_jurnal = $tanggal_jurnal;
+                $header->void = $void;
+                $header->catatan = $catatan_me;
+                $header->user_modified = $user_created;
+                $header->dt_modified = date('Y-m-d h:i:s');
+            } else {
+                $header = new JurnalHeader();
+                $header->id_cabang = $id_cabang;
+                $header->kode_jurnal = $this->generateJournalCode($id_cabang, 'ME');
+                $header->id_transaksi = $id_transaksi;
+                $header->jenis_jurnal = 'ME';
+                $header->tanggal_jurnal = $tanggal_jurnal;
+                $header->void = $void;
+                $header->catatan = $catatan_me;
+                $header->user_created = $user_created;
+                $header->dt_created = date('Y-m-d h:i:s');
+                $header->user_modified = $user_created;
+                $header->dt_created = date('Y-m-d h:i:s');
+            }
+
+            if (!$header->save()) {
+                DB::rollback();
+                return response()->json([
+                    "result" => false,
+                    "code" => 400,
+                    "message" => "Error when store Jurnal data on table header"
+                ]);
+            }
+
+            if (!empty($jurnal_detail_me)) {
+                $index = 1;
+                foreach ($jurnal_detail_me as $jd) {
+                    if(($jd['debet'] > 0 && $jd['credit'] == 0) || ($jd['debet'] == 0 && $jd['credit'] > 0)){
+                        $detail = new JurnalDetail();
+                        $detail->id_jurnal = $header->id_jurnal;
+                        $detail->index = $index;
+                        $detail->id_akun = $jd['akun'];
+                        $detail->debet = $jd['debet'];
+                        $detail->credit = $jd['credit'];
+                        $detail->keterangan = $jd['keterangan'];
+                        $detail->id_transaksi = $jd['id_transaksi'];
+                        $detail->user_created = $user_created;
+                        $detail->dt_created = date('Y-m-d h:i:s');
+                        $detail->user_modified = $user_created;
+                        $detail->dt_modified = date('Y-m-d h:i:s');
+
+                        Log::debug(json_encode($detail));
+
+                        if (!$detail->save()) {
+                            DB::rollback();
+                            return response()->json([
+                                "result" => false,
+                                "code" => 400,
+                                "message" => "Error when store Jurnal data on table detail"
+                            ]);
+                        }
+
+                        $index++;
+                    }
+                }
+            }
+
+            if($id_slip != null){
+                // init slip
+                $data_slip = Slip::find($id_slip);
+
+                if ($data_slip->jenis_slip == 0) {
+                    $jurnal_type = 'KM';
+                    $jurnal_type_detail = 'Kas Masuk';
+                } else if ($data_slip->jenis_slip == 1) {
+                    $jurnal_type = 'BM';
+                    $jurnal_type_detail = 'Bank Masuk';
+                } else {
+                    return response()->json([
+                        "result" => false,
+                        "code" => 400,
+                        "message" => "Error, please use slip Kas Masuk or Bank Masuk"
+                    ]);
+                }
+
+                $akun_slip = $data_slip->id_akun;
+
+                $jurnal_detail_pelunasan = [
+                    [
+                        'akun'          => $akun_slip,
+                        'debet'         => $total,
+                        'credit'        => 0,
+                        'keterangan'    => 'Jurnal Otomatis ' . $jurnal_type_detail . ' Pelunasan - ' . $id_transaksi . ' - ' . $nama_pelanggan,
+                        'id_transaksi'  => null
+                    ],
+                    [
+                        'akun'          => $akun_piutang_dagang,
+                        'debet'         => 0,
+                        'credit'        => $total,
+                        'keterangan'    => 'Jurnal Otomatis Pelunasan - ' . $id_transaksi . ' - ' . $nama_pelanggan,
+                        'id_transaksi'  => $id_transaksi
+                    ]
+                ];
+
+                // Find Header data and delete detail
+                $header = JurnalHeader::where("id_transaksi", $id_transaksi)->where('void', 0)->first();
+
+                // Begin save
+                DB::beginTransaction();
+                if (!empty($header) && $header->id_slip != $id_slip) {
+                    JurnalDetail::where('id_jurnal', $header->id_jurnal)->delete();
+                    $header->id_cabang = $id_cabang;
+                    $header->tanggal_jurnal = $tanggal_jurnal;
+                    $header->void = $void;
+                    $header->catatan = $catatan_me;
+                    $header->user_modified = $user_created;
+                    $header->dt_modified = date('Y-m-d h:i:s');
+                } else {
+                    if(!empty($header) && $header->id_slip == $id_slip){
+                        $header->void = 1;
+                        $header->save();
+                    }
+                    $header = new JurnalHeader();
+                    $header->id_cabang = $id_cabang;
+                    $header->kode_jurnal = $this->generateJournalCode($id_cabang, $jurnal_type, $id_slip);
+                    $header->id_transaksi = $id_transaksi;
+                    $header->jenis_jurnal = $jurnal_type;
+                    $header->tanggal_jurnal = $tanggal_jurnal;
+                    $header->void = $void;
+                    $header->catatan = $catatan_me;
+                    $header->user_created = $user_created;
+                    $header->dt_created = date('Y-m-d h:i:s');
+                    $header->user_modified = $user_created;
+                    $header->dt_created = date('Y-m-d h:i:s');
+                }
+
+                if (!$header->save()) {
+                    DB::rollback();
+                    return response()->json([
+                        "result" => false,
+                        "code" => 400,
+                        "message" => "Error when store Jurnal data on table header"
+                    ]);
+                }
+
+                if (!empty($jurnal_detail_pelunasan)) {
+                    $index = 1;
+                    foreach ($jurnal_detail_me as $jd) {
+                        if(($jd['debet'] > 0 && $jd['credit'] == 0) || ($jd['debet'] == 0 && $jd['credit'] > 0)){
+                            $detail = new JurnalDetail();
+                            $detail->id_jurnal = $header->id_jurnal;
+                            $detail->index = $index;
+                            $detail->id_akun = $jd['akun'];
+                            $detail->debet = $jd['debet'];
+                            $detail->credit = $jd['credit'];
+                            $detail->keterangan = $jd['keterangan'];
+                            $detail->id_transaksi = $jd['id_transaksi'];
+                            $detail->user_created = $user_created;
+                            $detail->dt_created = date('Y-m-d h:i:s');
+                            $detail->user_modified = $user_created;
+                            $detail->dt_modified = date('Y-m-d h:i:s');
+
+                            Log::debug(json_encode($detail));
+
+                            if (!$detail->save()) {
+                                DB::rollback();
+                                return response()->json([
+                                    "result" => false,
+                                    "code" => 400,
+                                    "message" => "Error when store Jurnal data on table detail"
+                                ]);
+                            }
+
+                            $index++;
+                        }
+                    }
+                }
+            }
+
+            DB::commit();
+            return response()->json([
+                "result" => true,
+                "code" => 200,
+                "message" => "Successfully stored Jurnal data",
+            ]);
+        // } catch (\Exception $e) {
+        //     DB::rollback();
+        //     Log::info("Error when store Jurnal data");
+        //     Log::info($e);
+        //     return response()->json([
+        //         "result" => false,
+        //         "code" => 400,
+        //         "message" => "Error when store Jurnal data",
+        //         "exception" => $e
+        //     ]);
+        // }
+    }
+
+    public function generateJournalCode($cabang, $jenis, $slip = null)
     {
         try {
             $ex = 0;
             do {
                 // Init data
                 $kodeCabang = Cabang::find($cabang);
-                $kodeSlip = Slip::find($slip);
-                $prefix = $kodeCabang->kode_cabang . "." . $jenis . "." . $kodeSlip->kode_slip . "." . date("ym");
+                if($slip != null){
+                    $kodeSlip = Slip::find($slip);
+                    $prefix = $kodeCabang->kode_cabang . "." . $jenis . "." . $kodeSlip->kode_slip . "." . date("ym");
+                }else{
+                    $prefix = $kodeCabang->kode_cabang . "." . $jenis . "." . date("ym");
+                }
 
                 // Check exist
                 $check = JurnalHeader::where("kode_jurnal", "LIKE", "$prefix%")->orderBy("kode_jurnal", "DESC")->get();
