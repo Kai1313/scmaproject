d1930e6b6dc1a4f02194a5cbc91091c46fee667e
ashabfirdaus <firdaus.ashab@gmail.com>
2023-04-17 10:14:22 +0700

    id_kategori_wrapper di modul master_wrapper

--- resources/views/ops/master/wrapper/form.blade.php
+++ resources/views/ops/master/wrapper/form.blade.php
@@ -32,14 +32,15 @@
         <div class="box">
             <div class="box-header">
                 <h3 class="box-title">{{ $data ? 'Ubah' : 'Tambah' }} Pembungkus</h3>
-                <a href="{{ route('master-wrapper') }}" class="btn bg-navy btn-sm btn-default btn-flat pull-right"><span
-                        class="glyphicon glyphicon-arrow-left mr-1" aria-hidden="true"></span> Kembali</a>
+                <a href="{{ route('master-wrapper') }}" class="btn bg-navy btn-sm btn-default btn-flat pull-right">
+                    <span class="glyphicon glyphicon-arrow-left mr-1" aria-hidden="true"></span> Kembali
+                </a>
             </div>
             <div class="box-body">
                 <form action="{{ route('master-wrapper-save-entry', $data ? $data->id_wrapper : 0) }}" method="post"
                     enctype="multipart/form-data" class="post-action">
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-4">
                             <div class="form-group">
                                 <label>Cabang <span>*</span></label>
                                 <select name="id_cabang" class="form-control select2" data-validation="[NOTEMPTY]"
@@ -60,6 +61,20 @@
                                     data-validation-message="nama pembungkus tidak boleh kosong">
                             </div>
                             <div class="form-group">
+                                <label>Kategori Pembungkus <span>*</span></label>
+                                <select name="id_kategori_wrapper" class="form-control select2">
+                                    <option value="">Pilih Kategori Pembungkus</option>
+                                    <option value="1" {{ old('id_kategori_wrapper') == '1' ? 'selected' : '' }}>
+                                        Palet
+                                    </option>
+                                    <option value="2" {{ old('id_kategori_wrapper') == '2' ? 'selected' : '' }}>
+                                        Zak
+                                    </option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
                                 <label>Berat <span>*</span></label>
                                 <div class="input-group">
                                     <input type="text" name="weight" class="form-control handle-number-4"
@@ -68,14 +83,17 @@
                                     <span class="input-group-addon">KG</span>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-md-6">
                             <div class="form-group">
                                 <label>Catatan</label>
                                 <textarea name="catatan" class="form-control" rows="4">{{ old('catatan', $data ? $data->catatan : '') }}</textarea>
                             </div>
+                        </div>
+                        <div class="col-md-4">
                             <div class="form-group">
                                 <label>Gambar</label>
+                                <input id="f_image" type="file" class="form-control" name="file_upload"
+                                    accept=".png,.jpeg,.jpg">
+                                <input type="hidden" name="image_path">
                                 <br>
                                 @if ($data && $data->path)
                                     <a href="{{ env('FTP_GET_FILE') . $data->path }}" target="_blank">
@@ -85,15 +103,11 @@
                                 @else
                                     <img alt="" width="100" id="uploadPreview1">
                                 @endif
-                                <br>
-                                <input id="f_image" type="file" class="form-control" name="file_upload"
-                                    accept=".png,.jpeg,.jpg">
-                                <input type="hidden" name="image_path">
                             </div>
                         </div>
                     </div>
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                    <button class="btn btn-primary btn-flat pull-right" type="submit">
+                    <button class="btn btn-primary btn-flat pull-right btn-sm" type="submit">
                         <i class="glyphicon glyphicon-floppy-saved"></i> Simpan Data
                     </button>
                 </form>
@@ -115,6 +129,8 @@
         $('[name="id_cabang"]').select2({
             data: branch
         })
+
+        $('.select2').select2()
 
         $('[name="file_upload"]').change(function() {
             let oFReader = new FileReader();
