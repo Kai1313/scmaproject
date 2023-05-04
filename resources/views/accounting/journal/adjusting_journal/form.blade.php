052bcae37851af95969740035d119720ec7c9938
Kaisha Satrio <kaishasatrio@gmail.com>
2023-05-04 22:43:01 +0700

    force 0 if <= 0

--- resources/views/accounting/journal/adjusting_journal/form.blade.php
+++ resources/views/accounting/journal/adjusting_journal/form.blade.php
@@ -1981,7 +1981,7 @@
         num = num.toString().replace(/\,/gi, "");
         num += '';
         x = num.split('.');
-        x1 = x[0];
+        x1 = (x[0] <= 0)?0:x[0];
         x2 = x.length > 1 ? ',' + x[1] : ',00';
         var rgx = /(\d+)(\d{3})/;
         while (rgx.test(x1)) {
