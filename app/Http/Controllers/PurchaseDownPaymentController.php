168e964421d1aec2030c71decb7cf0ab67c01702
ashabfirdaus <firdaus.ashab@gmail.com>
2023-05-13 06:17:12 +0700

    slip null uang muka pembelian

--- app/Http/Controllers/PurchaseDownPaymentController.php
+++ app/Http/Controllers/PurchaseDownPaymentController.php
@@ -154,7 +154,7 @@ class PurchaseDownPaymentController extends Controller
             $resultJurnalUangMukaPembelian = (new ApiController)->journalUangMukaPembelian(new Request([
                 "no_transaksi" => $data->kode_uang_muka_pembelian,
                 "tanggal" => $data->tanggal,
-                "slip" => '',
+                "slip" => null,
                 "cabang" => $data->id_cabang,
                 "pemasok" => $data->purchaseOrder->id_pemasok,
                 "void" => $data->void,
