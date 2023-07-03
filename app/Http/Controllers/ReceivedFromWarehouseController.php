bac0086e585c0185293fdca126b56d763b28e6d8
ashabfirdaus <firdaus.ashab@gmail.com>
2023-07-03 14:35:17 +0700

    void pindah ke gudang 2

--- app/Http/Controllers/ReceivedFromWarehouseController.php
+++ app/Http/Controllers/ReceivedFromWarehouseController.php
@@ -34,7 +34,7 @@ class ReceivedFromWarehouseController extends Controller
                 ->leftJoin('pindah_barang as pb2', 'pb.id_pindah_barang2', 'pb2.id_pindah_barang')
                 ->where('pb.id_jenis_transaksi', 24)
                 ->where('pb.type', 1)
-                ->where('status_pindah_barang', '!=', '2');
+                ->where('pb.status_pindah_barang', '!=', '2');
             if (isset($request->c)) {
                 $data = $data->where('pb.id_cabang', $request->c);
             }
