380ef31b537566ad4c51e04bba1c76df9abad99f
ashabfirdaus <firdaus.ashab@gmail.com>
2023-07-11 08:53:06 +0700

    mengembalikan sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1225,7 +1225,7 @@
                 </a>
                 <ul class="treeview-menu">
                     <li class="{{ request()->segment(2) == 'general_ledger' ? 'active' : null }}">
-                        <a href="{{ route('transaction-general-ledger') }}">Jurnal Umum</a>
+                        <a href="{{ route('transaction-general-ledger', 1) }}">Jurnal Umum</a>
                     </li>
                     <li class="{{ request()->segment(2) == 'adjustment_ledger' ? 'active' : null }}">
                         <a href="{{ route('transaction-adjustment-ledger') }}">Jurnal Penyesuaian</a>
