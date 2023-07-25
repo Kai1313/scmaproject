06d186cdf7ff24b8c9c83a3ee2bad53511737be3
Aris Saputra <arissaputra362@gmail.com>
2023-07-25 22:45:35 +0700

    fix redirect dari laporan neraca dan labarugi

--- app/Http/Controllers/ReportProfitAndLossController.php
+++ app/Http/Controllers/ReportProfitAndLossController.php
@@ -1041,6 +1041,7 @@ class ReportProfitAndLossController extends Controller
                         $array_item = [
                             'header' => $newHeader4,
                             'akun' => $item['id_akun'],
+                            'kode_akun' => $item['kode_akun'],
                             'start_date' => $start_date,
                             'end_date' => $end_date,
                         ];
