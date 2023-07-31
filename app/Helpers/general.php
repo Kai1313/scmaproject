fe0c03da2f0ad4d4d3b0cf25022f71c484c8fc74
Nove Anando <nove.anando@gmail.com>
2023-07-31 17:30:20 +0700

    update

--- app/Helpers/general.php
+++ app/Helpers/general.php
@@ -30,7 +30,6 @@ function checkUserSession($request, $alias_menu, $type)
         if (session()->has('token') == true) {
             $user_id = session()->get('user')->id_pengguna;
         }
-
         $user = User::where('id_pengguna', $user_id)->first();
         $token = UserToken::where('id_pengguna', $user_id)->where('status_token_pengguna', 1)->whereRaw("waktu_habis_token_pengguna > STR_TO_DATE(?, '%Y-%m-%d %H:%i:%s')", \Carbon\Carbon::now()->format('Y-m-d H:i:s'))->first();
 
