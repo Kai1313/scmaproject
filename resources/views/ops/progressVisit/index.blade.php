470e13dc80a338019174831f6f9591af506e6364
Nove Anando <nove.anando@gmail.com>
2023-08-01 11:22:25 +0700

    update

---
+++ resources/views/ops/progressVisit/index.blade.php
@@ -0,0 +1,130 @@
+@extends('layouts.main')
+@section('addedStyles')
+    <link rel="stylesheet" href="{{ asset('assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/bower_components/datatables-responsive/css/responsive.dataTables.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/bower_components/select2/dist/css/select2.min.css') }}">
+    <style>
+        ul.horizontal-list {
+            min-width: 200px;
+            list-style: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        ul.horizontal-list li {
+            display: inline;
+        }
+
+        .mb-1 {
+            margin-bottom: .25rem !important;
+        }
+
+        th {
+            text-align: center;
+        }
+
+        .head-checkbox {
+            padding-top: 30px;
+        }
+
+        .head-checkbox label {
+            margin-right: 10px;
+        }
+
+        .rounded-0 {
+            border-radius: 0;
+        }
+    </style>
+@endsection
+
+@section('header')
+    <section class="content-header">
+        <h1>
+            Progress Visit
+            <small></small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+            <li class="active">Progress Visit</li>
+        </ol>
+    </section>
+@endsection
+
+@section('main-section')
+    <div class="content container-fluid">
+        <div class="box">
+            <div class="box-header">
+                <div class="row">
+                    <div class="col-md-4">
+                        <label>Cabang</label>
+                        <div class="form-group">
+                            <select name="id_cabang" class="form-control select2">
+                                @foreach ($cabang as $branch)
+                                    <option value="{{ $branch['id'] }}">{{ $branch['text'] }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <label>&nbsp</label>
+                        <div class="form-group">
+                            <a href="{{ route('pre_visit-entry') }}"
+                                class="btn btn-success pull-right btn-flat btn-sm mr-1">
+                                <i class="glyphicon glyphicon-plus"></i> Progress Visit
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="box-body">
+                <div class="table-responsive">
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('addedScripts')
+    <script src="{{ asset('assets/bower_components/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/bower_components/datatables-responsive/js/dataTables.responsive.js') }}"></script>
+    <script src="{{ asset('assets/bower_components/select2/dist/js/select2.min.js') }}"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="{{ asset('js/custom.js') }}"></script>
+@endsection
+
+@section('externalScripts')
+    <script>
+        $('.select2').select2();
+        var table = $('.data-table').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "{{ route('pre_visit') }}",
+            columns: [{
+                data: 'visit_code',
+                name: 'v.visit_code'
+            }, {
+                data: 'visit_date',
+                name: 'v.visit_date'
+            }, {
+                data: 'nama_salesman',
+                name: 's.nama_salesman',
+            }, {
+                data: 'nama_pelanggan',
+                name: 'p.nama_pelanggan',
+            }, {
+                data: 'alamat_pelanggan',
+                name: 'p.alamat_pelanggan',
+            }, {
+                data: 'pre_visit_desc',
+                name: 'v.pre_visit_desc'
+            }, {
+                data: 'action',
+                name: 'action',
+                className: 'text-center',
+                orderable: false,
+                searchable: false
+            }, ]
+        });
+    </script>
+@endsection
