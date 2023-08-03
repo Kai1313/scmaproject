02375580fa071420d81f213df16b4cb715adf473
Aris Saputra <arissaputra362@gmail.com>
2023-08-04 00:01:54 +0700

    fix closing jurnal
    fix jurnal closing hpp

--- app/Models/Transaction/ProductionDetail.php
+++ app/Models/Transaction/ProductionDetail.php
@@ -8,6 +8,5 @@ class ProductionDetail extends Model
 {
     protected $table = 'produksi_detail';
     protected $primaryKey = 'id_produksi_detail';
-    const CREATED_AT = 'dt_created';
-    const UPDATED_AT = 'dt_modified';
+    public $timestamps = false;
 }
