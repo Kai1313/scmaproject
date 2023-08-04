e412cb89da326ae2b0b768bf5ee360dd895fa5c9
Nove Anando <nove.anando@gmail.com>
2023-08-04 08:02:43 +0700

    update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3341,8 +3341,7 @@ class ApiController extends Controller
             ->join('barang AS brg', 'brg.id_barang', '=', 'b.id_barang')
             ->where('bd.id_barang', $id_barang)
             ->where('b.status_bom', 1)
-            ->groupBy('bd.id_barang', 'b.id_bom')->get();
-        dd($childsub);
+            ->groupBy('bd.id_barang', 'b.id_bom');
         $child = \DB::table(\DB::raw("({$childsub->toSql()}) as a"))
             ->select('a.*', \DB::raw('AVG(a.prorate) AS avg_prorate'))
             ->groupBy('a.id_barang')->get();
