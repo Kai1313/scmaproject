bb6d68e1ce86d21fbbaa673869e80bcc09b10573
ashabfirdaus <firdaus.ashab@gmail.com>
2023-09-26 15:53:22 +0700

    lanjutan

--- public/js/custom.js
+++ public/js/custom.js
@@ -206,7 +206,6 @@ function getCookie(cname) {
 
 var _cf = (function () {
     function _shift(x) {
-        console.log(x)
         var parts = x.toString().split('.');
         return (parts.length < 2) ? 1 : Math.pow(10, parts[1].length);
     }
