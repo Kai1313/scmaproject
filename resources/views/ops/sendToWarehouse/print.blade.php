baf8c3730bc05c4ba8545cbf1b31adda43be11ed
ashab firdaus <firdaus.ashab@gmail.com>
2024-06-10 15:35:32 +0700

    perbaikan font kirim ke gudang

--- resources/views/ops/sendToWarehouse/print.blade.php
+++ resources/views/ops/sendToWarehouse/print.blade.php
@@ -7,6 +7,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Surat jalan pindah cabang {{ $data->kode_pindah_barang }}</title>
     <style type="text/css">
+        * {
+            font-family: Arial;
+        }
+
         .table {
             border-collapse: collapse;
             width: 100%;
