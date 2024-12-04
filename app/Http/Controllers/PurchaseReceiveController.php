7356fbf8040a52fcfb2b63cb6725bba718ad20d1
ashab firdaus <firdaus.ashab@gmail.com>
2024-12-04 10:44:04 +0700

    tambah fitur cetak masal qrcode penerimaan barang

---
+++ app/Http/Controllers/PurchaseReceiveController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DB;
+use Illuminate\Http\Request;
+use PDF;
+
+class PurchaseReceiveController extends Controller
+{
+    public function printQrcode(Request $request, $id)
+    {
+        $param = [];
+        if ($request->detail) {
+            $param = json_decode($request->detail);
+        }
+
+        $data = DB::table('pembelian')->where('id_pembelian', $id)->first();
+        if (!$data) {
+            return abort(404);
+        }
+
+        $details = DB::table('pembelian_detail')
+            ->select(
+                'kode_batang_master_qr_code',
+                'nama_barang',
+                'nama_satuan_barang',
+                'jumlah_master_qr_code',
+                'sisa_master_qr_code',
+                'sg_master_qr_code',
+                'batch_master_qr_code',
+                DB::raw('master_qr_code.weight_zak + master_qr_code.weight as total_tare'),
+                'kode_batang_rak'
+            )
+            ->join('master_qr_code', 'kode_batang_pembelian_detail', 'kode_batang_master_qr_code')
+            ->join('barang', 'master_qr_code.id_barang', 'barang.id_barang')
+            ->join('satuan_barang', 'master_qr_code.id_satuan_barang', 'satuan_barang.id_satuan_barang')
+            ->leftJoin('rak', 'master_qr_code.id_rak', 'rak.id_rak')
+            ->where('pembelian_detail.id_pembelian', $id);
+        if ($param) {
+            $details = $details->whereIn('id_pembelian_detail', $param);
+        }
+
+        $details = $details->get();
+
+        if (count($details) > 0) {
+            $mpdf = PDF::loadView('ops.purchaseReceive.print', ['data' => $data, 'details' => $details]);
+            $mpdf->setPaper([0, 0, 283.465, 113.386], 'portrait');
+            $mpdf->output();
+
+            return $mpdf->stream('Qrcode Hasil Penerimaan ' . $data->nama_pembelian . '.pdf');
+        }
+
+        return abort(404);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -208,6 +208,7 @@ Route::get('kirim_ke_gudang/print2/{id}', 'SendToWarehouseController@print2')->n
 Route::get('kirim_ke_gudang/print/{id}', 'SendToWarehouseController@print')->name('send_to_warehouse-print');
 Route::get('delivery_order/print-npb/{id}', 'DeliveryOrderController@printNpb')->name('delivery_order-print-npb');
 Route::get('production/print-hp/{id}', 'ProductionController@printHp')->name('production-print-hp');
+Route::get('purchase-receive/print-qrcode/{id}', 'PurchaseReceiveController@printQrcode')->name('purchase_receive-print-qrcode');
 Route::get('stok_minimal/excel/{id}/{id_cabang}', 'StokMinHistoryController@getExcel')->name('stok_minimal-excel');
 
 Route::namespace('Report')->group(function () {
